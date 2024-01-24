af906994fabe50acf133a1c77cd90a408e92a1f7
murukai.mberi <murukai.mberi@wonderlabz.com>
2024-01-24 09:06:16 +0200

    first commit

---
+++ src/test/java/com/afrikatek/churchconfigserver/ChurchConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afrikatek.churchconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
